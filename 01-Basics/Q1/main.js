9597da0799db2bcce20d7eb1b1e430bd57334780
Kaai Suzuki <suzydp@me.com>
2018-10-14 16:33:09 -0700

    Answered - Basics Q1

--- 01-Basics/Q1/main.js
+++ 01-Basics/Q1/main.js
@@ -1,3 +1,21 @@
 'use strict';
-const time = new Date();
-console.log(time);
+let head;
+let date = new Date();//get whole information of current time
+let day = date.getDay()//get current day
+let hour = date.getHours();//get current hours
+let minute = date.getMinutes();//get current minutes
+let second = date.getSeconds();
+
+//make array to show day properly
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//identify AM or PM
+if (hour >= 12){
+    hour = hour-12;
+    head = "PM";
+}else{
+    head = "AM";
+}
+
+document.getElementById("today").innerHTML = (weekday[day]);
+document.getElementById("currentTime").innerHTML = (hour + head + " : " + minute + " : " + second);
