b6a875b332cc63a9e4cb193a588061fb854e6864
Kaai Suzuki <suzydp@me.com>
2018-11-05 10:57:52 -0800

    WIP - Answered - Function Q3

--- 04-Functions/Q3/main.js
+++ 04-Functions/Q3/main.js
@@ -1,30 +1,16 @@
 'use strict';
 
-const fullDate = new Date();
+let arr = [];
+let string = "dog";
 
-function currentday(){
-    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = fullDate.getDay();
-    return "Today is : " + weekday[day];
+for(let i = 0; i<string.length; i++){
+    //// var result1 = string.substr(0, 1) + string.substr(0, 2) + string.substr(0, 3);
+    var result1 = string.substr(i, i+string.length-2);
+    var result2 = string.substr(i, i+string.length-1);
+    var result3 = string.substr(i, i+string.length);
+    // for(let r = 0; r>string.length; i--){
+    //     var result = string.substr(r, r + string.length-i);
+    // }
+    arr.push(result1, result2, result3);
 }
-
-document.getElementById("today").innerHTML = currentday();
-
-
-function currentTime(){
-    let head;
-    let hour = fullDate.getHours();
-    let min = fullDate.getMinutes();
-    let sec = fullDate.getSeconds();
-
-    if (hour >= 12) {
-        hour = hour - 12;
-        head = "PM";
-    } else {
-        head = "AM";
-    }
-
-    return "Current time is : " + hour + head + " : " + min + " : " + sec;
-}
-
-document.getElementById("currentTime").innerHTML = currentTime();
+console.log(arr);
